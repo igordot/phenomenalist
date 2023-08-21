aad12a004ca1f7ffad4e7d56330be5259114e2ef
igor <6363505+igordot@users.noreply.github.com>
2023-08-21 17:50:37 -0400

    Add tests

--- tests/testthat/test-create-object.R
+++ tests/testthat/test-create-object.R
@@ -1,11 +1,11 @@
-test_that("default import tonsil data", {
+test_that("import tonsil data default", {
   tonsil_csv <- system.file("extdata", "tonsil-akoya-2018-500.csv", package = "phenomenalist")
   tonsil_spe <- suppressMessages(create_object(tonsil_csv))
   expect_s4_class(tonsil_spe, "SpatialExperiment")
   expect_equal(ncol(tonsil_spe), 500)
 })
 
-test_that("import tonsil data with parameters", {
+test_that("import tonsil data with skip_cols and transformation", {
   tonsil_csv <- system.file("extdata", "tonsil-akoya-2018-500.csv", package = "phenomenalist")
   tonsil_spe <- suppressMessages(create_object(tonsil_csv, skip_cols = "DAPI|Blank", transformation = "z"))
   expect_s4_class(tonsil_spe, "SpatialExperiment")
@@ -14,7 +14,16 @@ test_that("import tonsil data with parameters", {
   expect_equal(length(assayNames(tonsil_spe)), 3)
 })
 
-test_that("import spleen data", {
+test_that("import spleen data default", {
+  spleen_csv <- system.file("extdata", "spleen-goltsev-2018-500.csv", package = "phenomenalist")
+  spleen_spe <- suppressMessages(create_object(spleen_csv))
+  expect_s4_class(spleen_spe, "SpatialExperiment")
+  expect_equal(nrow(spleen_spe), 32)
+  expect_equal(ncol(spleen_spe), 500)
+  expect_equal(length(assayNames(spleen_spe)), 1)
+})
+
+test_that("import spleen data with skip_cols", {
   spleen_csv <- system.file("extdata", "spleen-goltsev-2018-500.csv", package = "phenomenalist")
   spleen_spe <- suppressMessages(create_object(spleen_csv, skip_cols = "blank"))
   expect_s4_class(spleen_spe, "SpatialExperiment")
